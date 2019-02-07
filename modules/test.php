eba5a2d52ef73d849532bfa8a1cd199daa03645f
Antti Partanen <aehparta@iki.fi>
2019-02-07 12:03:37 +0200

    example console stuff

--- modules/test.php
+++ modules/test.php
@@ -1,7 +1,17 @@
 <?php
 
-function test()
+function exampleCommand($cmd, $args, $options)
 {
-    echo "moi\n";
+    log_info('You executed example call, command is: ' . $cmd);
+    log_notice('Action given: ' . $args['action']);
+    log_notice('Optional given: ' . $args['optional']);
+    log_warning('Files given:');
+    foreach ($args['files'] as $file) {
+        log_warning(' - ' . $file);
+    }
+    log_error('Options:');
+    log_error(' - int: ' . $options['int']);
+    log_error(' - do: ' . ($options['do'] ? 'yes' : 'no'));
+    log_error(' - string: ' . $options['string']);
     return true;
 }
