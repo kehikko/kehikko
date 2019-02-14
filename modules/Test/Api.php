bbc7b6169b58e0f37b6a79d83b4710217b860e3c
Antti Partanen <aehparta@iki.fi>
2019-02-14 15:27:31 +0200

    api testing

--- modules/Test/Api.php
+++ modules/Test/Api.php
@@ -4,17 +4,58 @@ namespace Test;
 
 class Api
 {
+    private $id;
+    private $type = null;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
     public function test()
     {
-        $_SERVER['REQUEST_METHOD'] = 'PUT';
+        $data = ['integer' => '999', 'type' => 'gas', 'time' => date_create('2019-01-01T18:00:00'), 'other' => 'no appear in final data'];
 
-        $data = ['integer' => 9, 'type' => 'gas', 'other' => 'no appear in final data'];
+        // $routes = route_init();
 
-        $routes = route_init();
+        // $_SERVER['REQUEST_METHOD'] = 'PUT';
+        // $out = api_validate($routes['base']['api'], $data);
+        // var_dump($out);
 
-        $data = api_validate($routes['base']['api'], $data);
-        var_dump($data);
+        // $_SERVER['REQUEST_METHOD'] = 'GET';
+        // $in = api_read($routes['base']['api'], $data);
+        // var_dump($in);
 
-        return true;
+        return $data;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getInteger()
+    {
+        return 976;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type)
+    {
+        $this->type = $type;
+    }
+
+    public function getTime()
+    {
+        return date_create();
+    }
+
+    public function getOther($o, $some, $other)
+    {
+        return $o->getId() . '-' . $some . '-' . $other;
     }
 }
